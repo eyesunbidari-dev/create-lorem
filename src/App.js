335ccb40054ce4795105cf8be8afb336a0fe7b16
aysan <aysanbidariiii@gmail.com>
2022-07-11 03:31:00 +0430

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import data from "./data";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handelerSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 4) {
+      amount = 4;
+    }
+    setText(data.slice(0, amount));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="section-center">
+      <h4>لورم ساز فارسی</h4>
+      <form className="lorem" onSubmit={handelerSubmit}>
+        <label htmlFor="total">تعداد پاراگراف</label>
+        <input
+          type="number"
+          value={count}
+          name="total"
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit"> بساز</button>
+      </form>
+      <article>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
     </div>
   );
 }
